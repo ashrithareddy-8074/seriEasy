c18627626c80ef06c272923b227da885c1cd1c75
Satwika Kollati <21je0474@iitism.ac.in>
2022-12-04 19:45:03 +0530

    sanitize-html

--- app.js
+++ app.js
@@ -34,11 +34,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 const sessionConfig = {
+  name:'session',
   secret: 'thisshouldbeabettersecret',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure:true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
